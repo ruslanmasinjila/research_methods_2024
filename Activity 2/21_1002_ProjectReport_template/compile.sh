1df56ecbad1fec300a1ab5eba7e6d1330ea4b7e4
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-10-29 22:35:17 -0230

    customized compilation script

---
+++ Activity 2/21_1002_ProjectReport_template/compile.sh
@@ -0,0 +1,14 @@
+echo 'performing 1/4 pass...'
+pdflatex -interaction=nonstopmode MainLatex.tex > /dev/null 2>&1
+echo 'complete'
+echo 'performing 2/4 pass...'
+bibtex MainLatex > /dev/null 2>&1
+echo 'complete'
+echo 'performing 3/4 pass...'
+pdflatex -interaction=nonstopmode MainLatex.tex > /dev/null 2>&1
+echo 'complete'
+echo 'performing 4/4 pass...'
+pdflatex -interaction=nonstopmode MainLatex.tex > /dev/null 2>&1
+echo 'complete. cleaning up'
+rm *.aux *.bbl *.blg *.log *.lof 
+echo 'done'
